380e31a74d2f6f12b96e69bcc961432d0e8ba6c2
Elizabeth Morgan Dixon <_@lizzie.io>
2016-11-21 11:15:20 +0000

    working backspace

--- src/typing.js
+++ src/typing.js
@@ -11,7 +11,18 @@ UI.prototype.right_char = function (c) {
 }
 
 UI.prototype.wrong_char = function (c) {
+    if (c) {
+	var text = new Text(c);
+	this.current_typing_div.appendChild(text);
+    }
     this.current_typing_div.className = "active-bad";
+}
+
+UI.prototype.backspace = function () {
+    if (this.current_typing_div.lastChild) {
+	this.current_typing_div.removeChild(
+	    this.current_typing_div.lastChild);
+    }
 }
 
 UI.prototype.clear = function () {
@@ -49,17 +60,36 @@ function Word(word, ui, finish) {
     this.remaining_word = word.split("");
     this.finish_fn = finish;
     this.ui = ui;
+    /* all keypresses and when they're pressed */
     this.log = [];
-    this.typos = 0;
+    /* number of bad keys pressed */
+    this.typos_total = 0;
+    /* how many backspaces are needed to get this in a good state. */
+    this.typos_current = 0;
+    this.start_time = null;
+    this.first_keypress_time = null;
 }
 
 Word.prototype.handle_keypress = function (ev) {
-    /* TODO should we allow typos? */
-    var code = String.fromCharCode(ev.charCode);
+    /* bad hack :( */
+    var code = "";
+    if (ev.key.length == 1) {
+	code = ev.key;
+    }
     this.log.push({code: code,
+		   key: ev.key,
 		   when: (new Date().getTime()),
 		   corect: code == this.remaining_word[0]});
-    if (code == this.remaining_word[0]) {
+    /* TODO avoid logging repeat characters? or what? */
+    if (ev.key == "Backspace") {
+	if (this.typos_current > 0) {
+	    this.typos_current -= 1;
+	    this.ui.backspace();
+	} else  {
+	    this.ui.wrong_char(code);
+	}
+    } else if (this.typos_current == 0 && code == this.remaining_word[0]) {
+	this.first_keypress_time |= new Date().getTime();
 	this.ui.right_char(this.remaining_word.shift());
 	if (this.remaining_word.length == 0) {
 	    /* TODO: send this */
@@ -68,24 +98,26 @@ Word.prototype.handle_keypress = function (ev) {
     } else {
 	/* make sure it's a char */
 	if (code.length > 0) {
-	    this.typos += 1;
+ 	    this.typos_total += 1;
+ 	    this.typos_current += 1;
 	    this.ui.wrong_char(code);
 	}
     }
 }
 
 Word.prototype.start = function () {
-    /* start listening to the mic */
+    /* TODO start listening to the mic */
     /* register keyboard callback */
-    document.addEventListener('keypress', this.handle_keypress.bind(this));
+    window.onkeydown = this.handle_keypress.bind(this);
     /* render self. */
+    this.start_time = new Date().getTime();
     this.ui.new_word(this.word);
 }
 
 Word.prototype.finish = function () {
-    /* stop listening on the mic */
+    /* TODO stop listening on the mic */
     /* kill the keyboard callback */
-    document.removeEventListener("keypress", this.handle_keypress.bind(this));
+    window.onkeydown = null;
     /* unrender self */
     this.ui.clear();
     /* call the callback! */
@@ -93,7 +125,7 @@ Word.prototype.finish = function () {
 }
 
 function main () {
-    /* TODO: collect other data about hte user, i.e. if it's a phone */
+    /* TODO: collect other data about hte user, i.e. if it's a phone, referer? */
     /* make sure current word div and current progress div are present.  */
     var words = ["def", "abc"];
     var ui = new UI(document.getElementById("current_word"),
