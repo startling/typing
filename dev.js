8fb6d5275ea6c58fd9fbfde02552ad24479c6d11
Elizabeth Morgan Dixon <_@lizzie.io>
2016-11-22 11:46:49 +0000

    uglifyify

--- dev.js
+++ dev.js
@@ -9,4 +9,5 @@ http.createServer(beefy({
     bundler: "./node_modules/browserify/bin/cmd.js",
     live: true,
     quiet: false,
+    bundlerFlags: ["--debug", "-t", "uglifyify"]
 })).listen(8080, "0.0.0.0");
