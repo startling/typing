8d64e7209934934eda2ea22bbfed3f4ac5d6ed45
Elizabeth Morgan Dixon <_@lizzie.io>
2016-11-22 10:42:09 +0000

    real build + publish

--- dev.js
+++ dev.js
@@ -2,8 +2,11 @@ var beefy = require('beefy');
 var http = require('http');
 
 http.createServer(beefy({
-    cwd: __dirname,
-    entries: ['src/typing.js'],
+    cwd: "resources",
+    entries: {
+	'/typing.js': 'src/typing.js'
+    },
+    bundler: "./node_modules/browserify/bin/cmd.js",
     live: true,
     quiet: false,
 })).listen(8080, "0.0.0.0");
